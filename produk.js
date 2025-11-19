ccbc041fad6027e455e5f12ea29a898b42b7f69d
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-11-19 17:56:15 +0800

    Create produk.js

---
+++ produk.js
@@ -0,0 +1,14 @@
+module.exports = function () {
+  return {
+    sections: [
+      {
+        title: "📦 Daftar Produk Store",
+        rows: [
+          { title: "Panel 1K", description: "Harga: 10K / bulan", rowId: "beli_panel1k" },
+          { title: "Panel 2GB", description: "Harga: 15K / bulan", rowId: "beli_panel2gb" },
+          { title: "Bot WhatsApp", description: "Harga: 20K / install", rowId: "beli_botwa" }
+        ]
+      }
+    ]
+  }
+}
