ee3c5c0b0c3dd42872d6aafd521dfd0d32551ab9
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-11-19 17:55:48 +0800

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+  return {
+    sections: [
+      {
+        title: "🛒 Produk Kami",
+        rows: [
+          { title: "📦 List Produk", rowId: "produk" }
+        ]
+      },
+      {
+        title: "💬 Bantuan",
+        rows: [
+          { title: "👤 Hubungi Admin", rowId: "admin" }
+        ]
+      }
+    ]
+  }
+}
